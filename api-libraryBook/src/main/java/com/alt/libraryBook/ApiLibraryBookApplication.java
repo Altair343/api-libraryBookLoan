a6d414956eda1b2292382ac0102e01f932118c3d
Andres Vergara <avergarap@iingen.unam.mx>
2026-01-02 15:28:23 -0600

    [ADD] Agregando plantilla

---
+++ api-libraryBook/src/main/java/com/alt/libraryBook/ApiLibraryBookApplication.java
@@ -0,0 +1,13 @@
+package com.alt.libraryBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLibraryBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLibraryBookApplication.class, args);
+	}
+
+}
---
+++ api-libraryBook/src/test/java/com/alt/libraryBook/ApiLibraryBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alt.libraryBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLibraryBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
